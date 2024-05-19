f3ce540c280abefb24a031e9596119cd979a9980
Andrey Elizarov <ergogen@ae.ayeaye.ae>
2024-05-19 14:26:36 +0400

    env.sh: update

--- env.sh
+++ env.sh
@@ -1,10 +1,28 @@
 
-__update_footprints() {
+__update_footprints_gui() {
   ./fetch-footprints.sh ../ergogen-footprints/my/ ../ergogen-footprints/vanilla/ --clear
   ./make-gui.sh sync
 }
 
-alias ff=__update_footprints
+__generate_pcb() {
+  ./fetch-footprints.sh ../ergogen-footprints/my/ ../ergogen-footprints/vanilla/ --clear && \
+
+  ./make-cli.sh run && \
+
+  open output/pcbs/clavier.kicad_pcb
+}
+
+__watch_and_gen_pcb() {
+  find input/ | entr -p bash -c "pkill pcbnew; source env.sh && __generate_pcb"
+}
+
+
+alias ff=__update_footprints_gui
+alias gg=__generate_pcb
+
+alias rgg=__watch_and_gen_pcb
+
+
 
 alias d=docker
 
